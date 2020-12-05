7d675957e54fb57eb677d95e20c9aa88c9661616
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2020-12-05 19:45:54 +0100

    Format source files

--- src/components/NavigationTabs.tsx
+++ src/components/NavigationTabs.tsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Tab, Tabs, TabHeading, Icon } from 'native-base';
+import React, {Component} from 'react';
+import {Tab, Tabs, TabHeading, Icon} from 'native-base';
 import Home from './Home';
 import Profile from './Profile';
 import Chat from './Chat';
@@ -7,17 +7,32 @@ import Chat from './Chat';
 export default class NavigationTabs extends Component {
   render() {
     return (
-        <Tabs locked>
-          <Tab heading={ <TabHeading><Icon name='account-circle' type="MaterialIcons" /></TabHeading>}>
-              <Profile/>
-          </Tab>
-          <Tab heading={ <TabHeading><Icon name='home' type="MaterialIcons" /></TabHeading>}>
-              <Home/>
-          </Tab>
-          <Tab heading={ <TabHeading><Icon name='chat-bubble' type="MaterialIcons" /></TabHeading>}>
-              <Chat/>
-          </Tab>
-        </Tabs>
+      <Tabs locked>
+        <Tab
+          heading={
+            <TabHeading>
+              <Icon name="account-circle" type="MaterialIcons" />
+            </TabHeading>
+          }>
+          <Profile />
+        </Tab>
+        <Tab
+          heading={
+            <TabHeading>
+              <Icon name="home" type="MaterialIcons" />
+            </TabHeading>
+          }>
+          <Home />
+        </Tab>
+        <Tab
+          heading={
+            <TabHeading>
+              <Icon name="chat-bubble" type="MaterialIcons" />
+            </TabHeading>
+          }>
+          <Chat />
+        </Tab>
+      </Tabs>
     );
   }
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,56 +1,105 @@
-import { Button, Col, Grid, Icon, Image, Row, Tab, Text, Thumbnail, View } from 'native-base';
+import {Button, Col, Grid, Icon, Row, Text, Thumbnail, View} from 'native-base';
 import React from 'react';
-import {
-  StyleSheet,
-} from 'react-native';
+import {StyleSheet} from 'react-native';
 
 const Profile = () => {
   const numberOfBooks = 21;
   const numberOfMatches = 11;
-  const userBiography = 'This is my biography, where I should tell the people I match which are my interests in books.'
+  const userBiography =
+    'This is my biography, where I should tell the people I match which are my interests in books.';
   return (
-	  <>
-      <Grid >
-        <Row size={0.75}  style={{ alignItems: 'center' }}>
-          <Col style={{ alignItems: 'center'}}>
-            <Thumbnail style={{width: 200, height: 200}} source={require('../assets/avatar.png')} />
+    <>
+      <Grid>
+        <Row size={0.75} style={{alignItems: 'center'}}>
+          <Col style={{alignItems: 'center'}}>
+            <Thumbnail
+              style={{width: 200, height: 200}}
+              source={require('../assets/avatar.png')}
+            />
             <Text style={{fontSize: 30, marginTop: 10}}>Francis Molina</Text>
-            <Text style={{fontSize: 20, margin: 5, textAlign: 'center'}}>{userBiography}</Text>
-            <View style={{ flexDirection: "row", justifyContent: 'space-between', padding: 15 }}>
-              <Button rounded style={{width: 150, margin: 10, flexDirection: "row", justifyContent: 'space-around'}}>
-                <Icon name="menu-book" type="MaterialIcons" style={{marginRight: 0}}/>
+            <Text style={{fontSize: 20, margin: 5, textAlign: 'center'}}>
+              {userBiography}
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                padding: 15,
+              }}>
+              <Button
+                rounded
+                style={{
+                  width: 150,
+                  margin: 10,
+                  flexDirection: 'row',
+                  justifyContent: 'space-around',
+                }}>
+                <Icon
+                  name="menu-book"
+                  type="MaterialIcons"
+                  style={{marginRight: 0}}
+                />
                 <Text>Books: {numberOfBooks}</Text>
               </Button>
-              <Button rounded style={{backgroundColor: 'orange', width: 150, margin: 10, flexDirection: "row", justifyContent: 'space-around'}}>
-                <Icon name="favorite" type="MaterialIcons" style={{marginRight: 0}}/>
+              <Button
+                rounded
+                style={{
+                  backgroundColor: 'orange',
+                  width: 150,
+                  margin: 10,
+                  flexDirection: 'row',
+                  justifyContent: 'space-around',
+                }}>
+                <Icon
+                  name="favorite"
+                  type="MaterialIcons"
+                  style={{marginRight: 0}}
+                />
                 <Text>Matches: {numberOfMatches}</Text>
               </Button>
             </View>
           </Col>
         </Row>
-        <Row size={0.25} style={{alignItems: 'center', justifyContent: "center"}}>
+        <Row
+          size={0.25}
+          style={{alignItems: 'center', justifyContent: 'center'}}>
           <Col>
-            <Button style={{backgroundColor: 'darkcyan',width: 320, alignSelf: 'center', flexDirection: "row", justifyContent: "center", margin: 5}}>
+            <Button
+              style={{
+                backgroundColor: 'darkcyan',
+                width: 320,
+                alignSelf: 'center',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                margin: 5,
+              }}>
               <Text>Edit Profile</Text>
             </Button>
-            <Button danger style={{width: 320, alignSelf: 'center', flexDirection: "row", justifyContent: "center", margin: 5}}>
+            <Button
+              danger
+              style={{
+                width: 320,
+                alignSelf: 'center',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                margin: 5,
+              }}>
               <Text>Log out</Text>
             </Button>
           </Col>
         </Row>
       </Grid>
-      
-	  </>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-	mainView: {
-		justifyContent: "center"
-	},
-	logo: {
-		width: 30,
-	}
+  mainView: {
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 30,
+  },
 });
 
 export default Profile;
