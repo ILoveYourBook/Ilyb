7ee3ab7c654dbf1bf3978c01fc8cbb9491657f32
rexuswolf <francisjmp99@gmail.com>
2020-12-07 19:19:20 +0100

    Refactoring CardSwiper

--- src/components/BookCardSwiper.tsx
+++ src/components/BookCardSwiper.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Body,
   Card,
@@ -7,53 +8,55 @@ import {
   Left,
   Text,
 } from 'native-base';
-import React, {Component} from 'react';
 import {Image, StyleSheet} from 'react-native';
-const books = [
-  {
-    name: 'La Sombra del Viento',
-    author: 'Carlos Ruiz Zafón',
-    genres: 'Drama, Mistery',
-    image: require('../assets/la-sombra.jpg'),
-  },
-  {
-    name: 'The Fellowship of the Ring',
-    author: 'J.R.R. Tolkien',
-    genres: 'Fantasy, Adventure',
-    image: require('../assets/fellowship.jpg'),
-  },
-];
-export default class BookCardSwiper extends Component {
-  render() {
-    return (
-      <Container style={styles.container}>
-        <DeckSwiper
-          dataSource={books}
-          renderItem={(item: any) => (
-            <Card style={{elevation: 3, width: 300, height: 453}}>
-              <CardItem cardBody>
-                <Image style={{height: 453, flex: 1}} source={item.image} />
-              </CardItem>
-              <CardItem style={{position: 'absolute', bottom: 0, opacity: 0.9}}>
-                <Left>
-                  <Body>
-                    <Text>{item.name}</Text>
-                    <Text>{item.author}</Text>
-                    <Text note>{item.genres}</Text>
-                  </Body>
-                </Left>
-              </CardItem>
-            </Card>
-          )}
-        />
-      </Container>
-    );
-  }
+
+export interface Props {
+  books: Array<any>;
 }
+
+export const BookCardSwiper: React.FunctionComponent<Props> = ({books}) => {
+  return (
+    <Container style={styles.container}>
+      <DeckSwiper
+        dataSource={books}
+        renderItem={(item: any) => (
+          <Card style={styles.card}>
+            <CardItem cardBody>
+              <Image style={styles.cardImg} source={item.image} />
+            </CardItem>
+            <CardItem style={styles.cardInfo}>
+              <Left>
+                <Body>
+                  <Text>{item.name}</Text>
+                  <Text>{item.author}</Text>
+                  <Text note>{item.genres}</Text>
+                </Body>
+              </Left>
+            </CardItem>
+          </Card>
+        )}
+      />
+    </Container>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 20,
     marginLeft: 30,
+  },
+  card: {
+    elevation: 4,
+    width: 300,
+    height: 454,
+  },
+  cardImg: {
+    height: 453,
+    flex: 1,
+  },
+  cardInfo: {
+    position: 'absolute',
+    bottom: 0,
+    opacity: 0.9,
   },
 });
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,28 +1,38 @@
-import {Icon, View} from 'native-base';
+import {Button, Icon, Text, View} from 'native-base';
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import BookCardSwiper from './BookCardSwiper';
+import {BookCardSwiper} from './BookCardSwiper';
+
+const books = [
+  {
+    name: 'La Sombra del Viento',
+    author: 'Carlos Ruiz Zafón',
+    genres: 'Drama, Mistery',
+    image: require('../assets/la-sombra.jpg'),
+  },
+  {
+    name: 'The Fellowship of the Ring',
+    author: 'J.R.R. Tolkien',
+    genres: 'Fantasy, Adventure',
+    image: require('../assets/fellowship.jpg'),
+  },
+];
 
 const Home = () => {
   return (
     <>
-      <BookCardSwiper />
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-          padding: 15,
-        }}>
-        <Icon
-          style={{fontSize: 40, color: 'black'}}
-          name="clear"
-          type="MaterialIcons"
-        />
-        <Icon
-          style={{fontSize: 40, color: 'red'}}
-          name="favorite"
-          type="MaterialIcons"
-        />
+      <BookCardSwiper books={books} />
+      <Button style={styles.infoBtn}>
+        <Icon name="info" type="MaterialIcons" style={styles.infoIcon} />
+        <Text>More Information</Text>
+      </Button>
+      <View style={styles.mainView}>
+        <Button style={styles.passBtn}>
+          <Icon style={styles.passIcon} name="clear" type="MaterialIcons" />
+        </Button>
+        <Button style={styles.likeBtn}>
+          <Icon style={styles.likeIcon} name="favorite" type="MaterialIcons" />
+        </Button>
       </View>
     </>
   );
@@ -30,10 +40,35 @@ const Home = () => {
 
 const styles = StyleSheet.create({
   mainView: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  infoBtn: {
+    width: 200,
+    alignSelf: 'center',
     justifyContent: 'center',
   },
-  logo: {
-    width: 30,
+  infoIcon: {
+    marginRight: 0,
+  },
+  passBtn: {
+    justifyContent: 'center',
+    backgroundColor: 'black',
+    borderRadius: 0,
+    flex: 0.5,
+  },
+  passIcon: {
+    fontSize: 40,
+  },
+  likeBtn: {
+    justifyContent: 'center',
+    backgroundColor: 'red',
+    borderRadius: 0,
+    flex: 0.5,
+  },
+  likeIcon: {
+    fontSize: 40,
   },
 });
 
