3fd17e429dfe56ac5d9b695b545372dce411ef10
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2020-12-12 14:08:11 +0100

    wip

--- src/components/UploadBookForm.tsx
+++ src/components/UploadBookForm.tsx
@@ -1,17 +1,24 @@
 /* eslint-disable no-shadow */
-import React from 'react';
-import {Controller, useForm} from 'react-hook-form';
-import {TextInput, Button, View} from 'react-native';
-import {Book} from './Home';
 import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
+import React, {useState} from 'react';
+import {Controller, useForm} from 'react-hook-form';
+import {Button, TextInput, View} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
+import uuid from 'uuid';
+import {Book} from './Home';
 
 const UploadBookForm = () => {
   const {control, handleSubmit} = useForm<Book>();
+  const [localPath, setLocalPath] = useState<string>();
 
   const onSubmit = async (book: Book) => {
+    const reference = storage().ref(uuid.v4());
+    await reference.putFile(localPath!);
     await firestore().collection('books').add({
       title: book.title,
       author: book.author,
+      image: reference.fullPath,
     });
   };
 
@@ -31,9 +38,7 @@ const UploadBookForm = () => {
       />
       <Controller
         name="author"
-        rules={{
-          required: true,
-        }}
+        rules={{required: true}}
         control={control}
         render={({onChange, value}) => (
           <TextInput
@@ -42,6 +47,14 @@ const UploadBookForm = () => {
             onChangeText={(value) => onChange(value)}
           />
         )}
+      />
+      <Button
+        title="Image"
+        onPress={() => {
+          launchImageLibrary({mediaType: 'photo'}, (response) => {
+            setLocalPath(response.uri);
+          });
+        }}
       />
       <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
