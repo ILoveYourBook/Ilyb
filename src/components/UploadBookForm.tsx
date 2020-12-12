c4e8a72a6d33628fa4a664f49052492930bbf3c5
rexuswolf <francisjmp99@gmail.com>
2020-12-12 17:27:57 +0100

    Camera photos upload working

--- src/components/UploadBookForm.tsx
+++ src/components/UploadBookForm.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-shadow */
 import firestore from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
+import {Row, Button, Text, Icon, Container, Grid} from 'native-base';
 import React, {useState} from 'react';
 import {Controller, useForm} from 'react-hook-form';
-import {Button, TextInput, View} from 'react-native';
-import {launchImageLibrary} from 'react-native-image-picker';
-import uuid from 'uuid';
+import {StyleSheet, TextInput, Image} from 'react-native';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import {Book} from './Home';
 
 const UploadBookForm = () => {
@@ -13,52 +13,112 @@ const UploadBookForm = () => {
   const [localPath, setLocalPath] = useState<string>();
 
   const onSubmit = async (book: Book) => {
-    const reference = storage().ref(uuid.v4());
-    await reference.putFile(localPath!);
-    await firestore().collection('books').add({
-      title: book.title,
-      author: book.author,
-      image: reference.fullPath,
-    });
+    try {
+      const reference = storage().ref(localPath);
+      await reference.putFile(localPath!);
+      await firestore().collection('books').add({
+        title: book.title,
+        author: book.author,
+        image: reference.fullPath,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <View>
-      <Controller
-        name="title"
-        rules={{required: true}}
-        render={({onChange, value}) => (
-          <TextInput
-            placeholder="Title"
-            value={value}
-            onChangeText={(value) => onChange(value)}
+    <Container style={styles.container}>
+      {/* {localPath ? (
+        <Image source={{uri: localPath}} />
+      ) : (
+        <Image source={require('../assets/image-preview.png')} />
+      )} */}
+      <Image source={require('../assets/image-preview.png')} />
+      <Grid>
+        <Row size={0.3}>
+          <Controller
+            name="title"
+            defaultValue=""
+            rules={{required: true}}
+            render={({onChange, value}) => (
+              <TextInput
+                style={styles.input}
+                placeholder="Title"
+                value={value}
+                onChangeText={(value) => onChange(value)}
+              />
+            )}
+            control={control}
           />
-        )}
-        control={control}
-      />
-      <Controller
-        name="author"
-        rules={{required: true}}
-        control={control}
-        render={({onChange, value}) => (
-          <TextInput
-            placeholder="Author"
-            value={value}
-            onChangeText={(value) => onChange(value)}
+        </Row>
+        <Row size={0.3}>
+          <Controller
+            name="author"
+            defaultValue=""
+            rules={{required: true}}
+            control={control}
+            render={({onChange, value}) => (
+              <TextInput
+                style={styles.input}
+                placeholder="Author"
+                value={value}
+                onChangeText={(value) => onChange(value)}
+              />
+            )}
           />
-        )}
-      />
-      <Button
-        title="Image"
-        onPress={() => {
-          launchImageLibrary({mediaType: 'photo'}, (response) => {
-            setLocalPath(response.uri);
-          });
-        }}
-      />
-      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
-    </View>
+        </Row>
+        <Row size={0.4}>
+          {/* <Button
+            style={styles.galleryButton}
+            onPress={async () => {
+              launchImageLibrary({mediaType: 'photo'}, (response) => {
+                setLocalPath(response.uri);
+              });
+            }}>
+            <Text>UPLOAD FROM GALLERY</Text>
+          </Button> */}
+          <Button
+            style={styles.cameraButton}
+            onPress={async () => {
+              launchCamera(
+                {mediaType: 'photo', saveToPhotos: true},
+                (response) => {
+                  setLocalPath(response.uri);
+                },
+              );
+            }}>
+            <Icon name="camera" type="MaterialCommunityIcons" />
+          </Button>
+        </Row>
+        <Row size={0.6}>
+          <Button
+            success
+            style={styles.submitButton}
+            onPress={handleSubmit(onSubmit)}>
+            <Text>SUBMIT</Text>
+          </Button>
+        </Row>
+      </Grid>
+    </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  input: {
+    width: 260,
+    height: 40,
+    backgroundColor: 'lightgray',
+    borderRadius: 6,
+    padding: 6,
+    margin: 4,
+    fontSize: 16,
+  },
+  galleryButton: {margin: 4},
+  cameraButton: {backgroundColor: 'gray', margin: 4},
+  submitButton: {margin: 4},
+});
 
 export default UploadBookForm;
