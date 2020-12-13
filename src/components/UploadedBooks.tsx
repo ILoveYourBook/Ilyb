f0e3c1c506ec1392e0ecf87a0e7f070058bb11ed
rexuswolf <francisjmp99@gmail.com>
2020-12-13 14:49:06 +0100

    Delete uploaded books working

--- src/components/UploadedBooks.tsx
+++ src/components/UploadedBooks.tsx
@@ -12,6 +12,7 @@ import {
   Right,
   Thumbnail,
   Text,
+  H1,
 } from 'native-base';
 import { Alert, StyleSheet } from 'react-native';
 
@@ -22,46 +23,76 @@ type Props = {
 const UploadedBooks = (props: Props) => {
   const [uploadedBooks, setUploadedBooks] = useState<Array<Book>>();
 
+  const getBookDocumentId = async (key: number): Promise<string> => {
+    try {
+      const data = await firestore()
+        .collection('books')
+        .where('userId', '==', props.user.id)
+        .get();
+
+      const documentId = data.docs[key].id;
+      return documentId;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const deleteBook = async (key: number) => {
+    try {
+      const documentId = await getBookDocumentId(key);
+      await firestore().collection('books').doc(documentId).delete();
+      await fetchBooks();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchBooks = async () => {
+    try {
+      const data = await firestore()
+        .collection('books')
+        .where('userId', '==', props.user.id)
+        .get();
+      const arrayData = data.docs.map((document) => document.data() as Book);
+      setUploadedBooks(arrayData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    const fetchBooks = async () => {
-      try {
-        const data = await firestore()
-          .collection('books')
-          .where('userId', '==', props.user.id)
-          .get();
-        const arrayData = data.docs.map((document) => document.data() as Book);
-        setUploadedBooks(arrayData);
-      } catch (error) {
-        console.log(error);
-      }
-    };
     fetchBooks();
   });
 
   const BookList = (books: Array<Book>) => {
     return (
-      <List>
-        {books.map((book, key: number) => {
-          return (
-            <ListItem key={key} onPress={() => Alert.alert('TBD')} thumbnail>
-              <Left>
-                <Thumbnail square source={{ uri: book.image }} />
-              </Left>
-              <Body>
-                <Text numberOfLines={1}>{book.title}</Text>
-                <Text note numberOfLines={1}>
-                  {book.author}
-                </Text>
-              </Body>
-              <Right>
-                <Button transparent>
-                  <Text style={styles.deleteButtonText}>Delete</Text>
-                </Button>
-              </Right>
-            </ListItem>
-          );
-        })}
-      </List>
+      <Container>
+        <H1 style={styles.header}>Your books</H1>
+        <List>
+          {books.map((book, key: number) => {
+            return (
+              <ListItem key={key} onPress={() => Alert.alert('TBD')} thumbnail>
+                <Left>
+                  <Thumbnail square source={{ uri: book.image }} />
+                </Left>
+                <Body>
+                  <Text numberOfLines={1}>{book.title}</Text>
+                  <Text note numberOfLines={1}>
+                    {book.author}
+                  </Text>
+                </Body>
+                <Right>
+                  <Button
+                    transparent
+                    onPress={async () => await deleteBook(key)}>
+                    <Text style={styles.deleteButtonText}>Delete</Text>
+                  </Button>
+                </Right>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Container>
     );
   };
 
@@ -73,6 +104,9 @@ const UploadedBooks = (props: Props) => {
 };
 
 const styles = StyleSheet.create({
+  header: {
+    margin: 20,
+  },
   deleteButtonText: {
     color: 'red',
   },
