423730c5c5cf4376efa6292a2df23b1c654bb6e3
rexuswolf <francisjmp99@gmail.com>
2020-12-14 20:18:38 +0100

    DetailedInfo improved
    Co-authored-by: AlexTheMagnus <malejandrolule@gmail.com>

--- src/components/DetailedInfo.tsx
+++ src/components/DetailedInfo.tsx
@@ -1,36 +1,67 @@
-import React from 'react';
-import { Container, Text, H1, H3 } from 'native-base';
-import { Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Container, Text, H1, H3, Grid, Row, Col, View } from 'native-base';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  TouchableWithoutFeedback,
+} from 'react-native';
 
 import { Book } from './Home';
 
 const DetailedInfo = (props: { book: Book }) => {
   const { book } = props;
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
   return (
-    <Container style={styles.container}>
-      <H1 style={styles.title}>{book.title}</H1>
-      <H3 style={styles.author}>{book.author}</H3>
-      <Image style={styles.img} source={{ uri: book.image }} />
-      <Text style={styles.opinion}>{book.opinion}</Text>
-    </Container>
+    <ScrollView>
+      <Container style={styles.container}>
+        <Modal
+          transparent={true}
+          animationType={'fade'}
+          visible={modalOpen}
+          onRequestClose={() => setModalOpen(false)}>
+          {
+            <TouchableWithoutFeedback onPress={() => setModalOpen(false)}>
+              <View style={styles.modalView}>
+                <Image style={styles.modalImage} source={{ uri: book.image }} />
+              </View>
+            </TouchableWithoutFeedback>
+          }
+        </Modal>
+        <Grid>
+          <Row size={0.25}>
+            <Col size={0.5}>
+              <H1 style={styles.title}>{book.title}</H1>
+              <H3 style={styles.author}>{book.author}</H3>
+            </Col>
+            <Col size={0.5}>
+              <TouchableOpacity onPress={() => setModalOpen(true)}>
+                <Image style={styles.img} source={{ uri: book.image }} />
+              </TouchableOpacity>
+            </Col>
+          </Row>
+          <Row size={0.05}>
+            <H3>Opinion</H3>
+          </Row>
+          <Row size={0.7}>
+            <Text>{book.opinion}</Text>
+          </Row>
+        </Grid>
+      </Container>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   img: {
-    height: 453,
+    width: '70%',
     aspectRatio: 3 / 4,
     alignSelf: 'center',
   },
   container: {
-    flex: 1,
-    alignItems: 'center',
-    padding: 20,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    padding: 25,
   },
   backBtn: {
     height: 80,
@@ -53,10 +84,15 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     marginTop: 10,
   },
-  opinion: {
-    marginBottom: 20,
-    marginTop: 10,
-    alignSelf: 'flex-start',
+  modalView: {
+    backgroundColor: 'rgba(255, 255, 255, .5)',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  modalImage: {
+    alignSelf: 'center',
+    width: '90%',
+    aspectRatio: 3 / 4,
   },
 });
 
